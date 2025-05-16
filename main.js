4dc906ec4d3d13b2b2d96eb9f6ac99e869aa483c
RomanTTTTT <tyshniuk.pn@ucu.edu.ua>
2025-05-16 15:05:15 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('side-menu');
+  
+    toggle.addEventListener('click', () => {
+      menu.classList.toggle('open');
+    });
+  });
+  
