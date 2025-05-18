0e470386883e8727c0261bf43586db09ac5b3ec0
RomanTTTTT <tyshniuk.pn@ucu.edu.ua>
2025-05-18 18:54:35 +0300

    added a cat picture

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import cat from './img/cat.jpg'
 
 function About() {
   return (
     <div className="container">
       <h2>Про нас</h2>
       <p>Це друга сторінка.</p>
+      <p>А це кіт.</p>
+      <img src={cat} alt="cat" style={{width:'200px', height:'auto'}} />
     </div>
   );
 }
